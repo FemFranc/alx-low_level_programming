a935f3b33766068de27b9433b39791dc353785f1
Olufemi Agbaje <olufemi.agbaje@yahoo.com>
2022-09-13 10:13:07 -0700

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,49 @@
 /*
- *  * File: 2-print_alphabet_x10.c
- *   * Auth: Brennan D Barab
- *    */
-#include "main.h"
+ *
+ *  * File: 0-holberton.c
+ *
+ *   * Auth: Brennan D Baraban
+ *
+ *    *
+ *
+ *     */
+
+#include "holberton.h"
+
 /**
  *
- *  * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ *  * main - Prints "Holberton" followed by a new line.
  *
- *   */
-void print_alphabet_x10(void)
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+
 {
-		int count = 0;
-			char letter
-				while (count++ <= 9)
-						{
-									for (letter = 'a'; letter <= 'z'; letter++)
-													_putchar(letter);
+
+		_putchar('H');
+
+			_putchar('o');
+
+				_putchar('l');
+
+					_putchar('b');
+
+						_putchar('e');
+
+							_putchar('r');
+
+								_putchar('t');
+
+									_putchar('o');
+
+										_putchar('n');
+
 											_putchar('\n');
-												}
+
+												return (0);
+
 }
