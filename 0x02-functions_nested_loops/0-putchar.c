d05b79921843100d72e846fca864e869a5b96016
Olufemi Agbaje <olufemi.agbaje@yahoo.com>
2022-09-13 11:58:26 -0700

    Nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
-/**
-* main - Prints "putchar" followed by a new line.
-* Return: Always 0.
+/** main - Prints putchar
+* Return: Always 0
 */
-
 int main(void)
 {
 		_putchar('_');
